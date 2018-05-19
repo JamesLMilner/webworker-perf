ac72606146a6aff1e49c4b214061c6812d684c8a
James Milner <jamesmilner22@hotmail.com>
2018-05-19 20:02:30 +0100

    Update github pages sites

--- docs/worker-stringify.js
+++ docs/worker-stringify.js
@@ -1,5 +1,6 @@
 onmessage = function(message) {
-    message.data = JSON.parse(message.data.data)
+    message.data.data = JSON.parse(message.data.data);
+    message.data.data = JSON.stringify(message.data.data)
     message.data.now = Date.now();
     postMessage(message.data);
 }
